6d9d9359ebe58abf9632eb9ee7dcc9ec90107a4f
evgeniy <pavlinl4@gmail.com>
2023-04-20 09:10:38 +0300

    .

--- keyworder/check_file_exist.py
+++ keyworder/check_file_exist.py
@@ -1,12 +1,17 @@
 from pathlib import Path
 
 
-def create_file_if_no(path_to_folder,file_name):
+def make_documents_subfolder(name):  # create folder "name" in User/Documents folder
+    (Path.home() / "Documents" / f"{name}").mkdir(parents=True, exist_ok=True)
+    return Path.home() / "Documents" / f"{name}"
+
+
+def create_file_if_no(subfolder_name, file_name):
+    path_to_folder = make_documents_subfolder(subfolder_name)
     Path(f'{path_to_folder}/{file_name}').touch(exist_ok=True)
     return f'{path_to_folder}/{file_name}'
 
 
 if __name__ == '__main__':
-    path_to_folder = f'{Path().home()}/Documents/keywords'
-    file_name = 'bad_words.txt'
-    print(create_file_if_no(file_name, path_to_folder))
+    create_file_if_no('subflder_name', 'file_name')
+
