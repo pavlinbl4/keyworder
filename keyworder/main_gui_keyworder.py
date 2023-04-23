552766b5107eb5f7b1d6c865bf3fc1d7b906ca2c
evgeniy <pavlinbl4@gmail.com>
2023-04-23 21:04:59 +0300

    added names to checkbox window

--- keyworder/main_gui_keyworder.py
+++ keyworder/main_gui_keyworder.py
@@ -13,29 +13,35 @@ import re
 if __name__ == '__main__':
     keyword_file = create_file_if_no('keywords', 'keywords in work.txt')
     bad_word_file = create_file_if_no('keywords', 'bad_words.txt')
+
     input_text = create_input_window()  # 1 get text from window
+
     durty_words = extract_words(input_text)  # 2 create list with all words in text
     durty_words_string = set_to_string(durty_words)
+
     #  remove bad words
     bad_words = bad_words_from_file(bad_word_file)
     durty_words_set = re.sub(bad_words, '', durty_words_string)
-
     durty_words_lema = lema(durty_words_string)  # make string for lema
     durty_words_set = set(durty_words_lema)  # remove double words
 
     # check keywords in GUI
-    word_for_work = create_checkbox_list(durty_words_set)  # select words for work -> list off  selected words
+    word_for_work = create_checkbox_list(durty_words_set,
+                                         window_name="Select keywords")  # select words for work -> list off  selected words
+
     # save words for work in text file
     write_keywords(",".join(word_for_work), keyword_file)
-    # print(f'{word_for_work = }')
+    print(f'{word_for_work = }')
 
     # now I want to check bad words
     unused_words = list_dif(durty_words_set, word_for_work)
-    bad_words = create_checkbox_list(unused_words)
-    add_bad_words_from_list(bad_words, bad_word_file)
+    print(f'{unused_words = }')
 
-    # print(f'{unused_words = }')
+    selected_bad_words = create_checkbox_list(unused_words, window_name='select "Bad Words"')
+    print(f'{selected_bad_words = }')
+
+    add_bad_words_from_list(selected_bad_words, bad_word_file)
+
     # copy words for work to clip memory
-
 
     # find not selected words and display this words to add some of tman to bad words
