df1156decc5dddab2aad576586a1b3e43c10124b
evgeniy <pavlinbl4@gmail.com>
2023-04-23 22:24:04 +0300

    fanction to clear text from doubles and bad words

---
+++ keyworder/optimize_input_text.py
@@ -0,0 +1,26 @@
+import re
+
+from bad_words.bad_words_file import bad_words_from_file
+from keyworder.check_file_exist import create_file_if_no
+from keyworder.lematization import lema
+from keyworder.set_to_string import set_to_string
+from keyworder.words_more_two_litters import extract_words
+
+
+def optimize_text(input_text, bad_words):
+    durty_words = extract_words(input_text)  # 2 create list with all words in text
+    durty_words_string = set_to_string(durty_words)  # create string with all words
+    durty_words_lema = lema(durty_words_string)  # lemmatize words and return list
+
+    durty_words_set = set(durty_words_lema)  # convert to set and remove doubles
+
+    durty_words_no_bad_words = re.sub(bad_words, '', set_to_string(durty_words_set))  # string
+
+    return {word for word in durty_words_no_bad_words.split(', ') if len(word) > 2}
+
+
+if __name__ == '__main__':
+    bad_word_file = create_file_if_no('keywords', 'bad_words.txt')
+    bad_words = bad_words_from_file(bad_word_file)
+    input_text = 'На заседании Законодательного Собрания Санкт-Петербурга с ежегодным отчетом выступил начальникF Главного управления Министерства юстиции РФ по Санкт-Петербургу и Ленинградской области Сергей Феоктистов.'
+    print(optimize_text(input_text, bad_words))
