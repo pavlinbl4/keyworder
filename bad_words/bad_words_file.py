377fcfa043fe339c18fc5995730b5b61459c0ac3
evgeniy <pavlinbl4@gmail.com>
2023-07-31 22:18:24 +0300

    refactoring

--- bad_words/bad_words_file.py
+++ bad_words/bad_words_file.py
@@ -5,11 +5,10 @@ this function add word from clip memory to the file with "bad" words
 import pyperclip
 from pathlib import Path
 import os
-from keyworder.check_file_exist import create_file_if_no
-
 from colorama import Fore
-from confirm_window.confirm_word import confirmation_window
+from gui_windows.confirm_word import confirmation_window
 from keyworder.notific import notification
+from kw_2023.check_file_exist import create_file_if_no
 
 
 def bad_words_from_file(file_path):
@@ -19,7 +18,6 @@ def bad_words_from_file(file_path):
     for i in lines:
         bad_words += '|' + i.strip()
     bad_word_from_file = f"\\b({bad_words.lstrip('|')})\\b"  # I don't remember why I create this f - stroke
-    # print(bad_word_from_file)
     return bad_word_from_file
 
 
@@ -28,7 +26,7 @@ def add_bad_word():
         new_bad_word = pyperclip.paste()
         if new_bad_word in bad_words_from_file(bad_word_file):
             print(f'{Fore.GREEN}{new_bad_word}{Fore.RESET}\n'
-                  f'уже сожержится в списке исключений')
+                  f'уже содержится в списке исключений')
         else:
             if confirmation_window(new_bad_word):
                 notification(new_bad_word, 'Добавлено исключение')
@@ -37,7 +35,7 @@ def add_bad_word():
                 text_file.write(new_bad_word + '\n')
 
 
-def add_bad_words_from_list(words_list, bad_word_file):
+def add_bad_words_from_list(words_list):
     with open(bad_word_file, 'a') as text_file:
         for word in words_list:
             if word not in bad_words_from_file(bad_word_file):
