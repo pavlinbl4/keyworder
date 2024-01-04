ee3e737db5bee006c6cde0f6d0fd6ccf0008dc27
evgenii <pavlinbl4@gmail.com>
2024-01-04 21:48:44 +0300

    add sorted()

--- keyworder/remove_doubles.py
+++ keyworder/remove_doubles.py
@@ -4,9 +4,11 @@ import re
 def no_doubles(no_bad_words):  # возвращает строку со словами разделенными пробелами
     re_cleaner = re.sub(r'\b(\w+)(\s+\1)', r'\1', re.sub(r'\W+', ' ', no_bad_words),
                         flags=re.I).strip()
-    return ", ".join([i for i in set(re_cleaner.split(' '))])
+    return ", ".join(sorted([i for i in set(re_cleaner.split(' '))]))
 
 
 if __name__ == '__main__':
-    print(no_doubles(
-        'коммерческий, недвижимость, свая, строительство, стройка,   парраы стройплощадка, коммерческий, недвижимость, свая, строительство, стройка, стройплощадка'))
+    print(no_doubles('слон, слон, dog, fox, dog'))
+
+    assert no_doubles('слон, слон, dog, fox, dog') == 'dog, fox, слон'
+    assert no_doubles('слон, слон, пес') == 'пес, слон'
