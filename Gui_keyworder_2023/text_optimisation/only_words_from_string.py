4e68a90b08a70cddfaf18902d3146e22fc12847a
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-03-16 19:00:25 +0300

    add option to convert tuple to string

--- Gui_keyworder_2023/text_optimisation/only_words_from_string.py
+++ Gui_keyworder_2023/text_optimisation/only_words_from_string.py
@@ -3,11 +3,18 @@
 import re
 
 
-def extract_only_words_from_string(text_string):
+def extract_only_words_from_string(text_string)-> list:
     if type(text_string) is list:
         text_string = ''.join(text_string)
+    if type(text_string) is tuple:
+        text_string = str(text_string)
+
     pattern = r'[А-Яа-яA-Za-z]+\-*[А-Яа-яA-Za-z]+'
     return re.findall(pattern, text_string)
+
+
+if __name__ == '__main__':
+    print(extract_only_words_from_string('Выборы президента'))
 
 
 
