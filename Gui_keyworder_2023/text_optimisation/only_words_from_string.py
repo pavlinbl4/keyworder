e21fdd8b96a911390e63e53148ba16eac084ff77
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-03-16 19:07:25 +0300

    use isinstance and modify pattern

--- Gui_keyworder_2023/text_optimisation/only_words_from_string.py
+++ Gui_keyworder_2023/text_optimisation/only_words_from_string.py
@@ -3,18 +3,13 @@
 import re
 
 
-def extract_only_words_from_string(text_string)-> list:
-    if type(text_string) is list:
-        text_string = ''.join(text_string)
-    if type(text_string) is tuple:
-        text_string = str(text_string)
+def extract_only_words_from_string(text_string) -> list:
+    if isinstance(text_string, (list, tuple)):
+        text_string = ' '.join(map(str, text_string))
 
-    pattern = r'[А-Яа-яA-Za-z]+\-*[А-Яа-яA-Za-z]+'
+    pattern = r'\b[-А-Яа-яA-Za-z]+\b'
     return re.findall(pattern, text_string)
 
 
 if __name__ == '__main__':
-    print(extract_only_words_from_string('Выборы президента'))
-
-
-
+    print(extract_only_words_from_string('Hello-World How-are-you?'))
