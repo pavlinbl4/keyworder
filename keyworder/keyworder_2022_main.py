1d04593f305d994553983ee87472affd3ca43fbf
evgeniy <pavlinbl4@gmail.com>
2024-08-25 22:34:45 +0300

    refactoring

--- keyworder/keyworder_2022_main.py
+++ keyworder/keyworder_2022_main.py
@@ -13,8 +13,8 @@ keyword_file = create_file_if_no(path_to_folder, 'keywords in work.txt')
 bad_word_file = create_file_if_no(path_to_folder, 'bad_words.txt')
 
 
-def bad_words_from_file():  # извлекаю слова исключения из текстового файла
-    with open(bad_word_file, 'r') as text_file:
+def bad_words_from_file(path_to_bad_word_file: str):  # извлекаю слова исключения из текстового файла
+    with open(path_to_bad_word_file, 'r') as text_file:
         lines = text_file.readlines()
     bad_words = ''
     for i in lines:
@@ -30,23 +30,19 @@ def write_keywords(final):
 
 
 def remove_doubles(no_bad_words):
-    # return re.sub(r'\b(\w+)(\s+\1)', r'\1', re.sub(r'\W+', ' ', no_bad_words),
-    #               flags=re.I).strip()  # удаляю повторы слов
     return ", ".join([word.strip() for word in set(no_bad_words) if word.isprintable()])
 
 
 def words_optimization():
-    durty_words = pyperclip.paste().lower()  # 1 keyword for optimisation from clip - string
-    # bad_words = bad_words_from_file()  # keywords from file that I want to delete
+    dirty_words = pyperclip.paste().lower()  # 1 keyword for optimisation from clip - string
+
     bad_words = 'черепаха'
-    optimization = re.findall(r'\w{3,}', durty_words)  # words longer 3 letters - list
+    optimization = re.findall(r'\w{3,}', dirty_words)  # words longer 3 letters - list
     optimization = (','.join(optimization))  # list to string
     # lemmatized_words = "".join(Mystem().lemmatize(optimization))  # string
     lemmatized_words = lema(optimization)  # list after lema
     no_doubles = remove_doubles(lemmatized_words)  # remove doubles
     final = re.sub(bad_words, '', no_doubles)  # удаляю слова паразиты
-
-    # final = re.sub(r'\s+', ', ', no_doubles)  # разделяю слова запятыми
 
     write_keywords(final)
     pyperclip.copy(final)
