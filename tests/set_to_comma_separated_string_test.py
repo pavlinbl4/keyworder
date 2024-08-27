bf40d0a57e3544b24f6c5d37181af5affb910e29
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-08-27 09:25:39 +0300

    added test with assertRaises method

--- tests/set_to_comma_separated_string_test.py
+++ tests/set_to_comma_separated_string_test.py
@@ -1,6 +1,6 @@
 from unittest import TestCase, main
 
-from Gui_keyworder_2023.text_optimisation.set_to_string import set_to_comma_separated_string
+from text_optimisation.set_to_string import set_to_comma_separated_string
 
 
 class SetToCommaString(TestCase):
@@ -14,10 +14,10 @@ class SetToCommaString(TestCase):
         input_set = {"apple", "banana", "cherry"}
         self.assertIsInstance(set_to_comma_separated_string(input_set), str)
 
-    def test_invalid_input_type(self):
-        invalid_input = [1, 2, 3]
+    def test_wrong_output_type(self):
+        input = ["apple", "banana", "cherry"]
         with self.assertRaises(TypeError):
-            set_to_comma_separated_string(invalid_input)
+            set_to_comma_separated_string(input)
 
 
 if __name__ == '__main__':
